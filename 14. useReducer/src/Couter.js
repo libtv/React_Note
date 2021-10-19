0c3fd472b69be1482baecbe605c2e42ceeef7a4a
Junho <api@sejongtelecom.net>
2021-10-19 10:54:29 +0900

    2021-10-19 useReducer

--- 14. useReducer/src/Couter.js
+++ 14. useReducer/src/Couter.js
@@ -1,19 +1,30 @@
-import react, { useState } from "react";
+import react, { useState, useReducer } from "react";
 
 function Counter() {
-    var [number, setNumber] = useState(0);
+    function reducer(state, action) {
+        switch (action.type) {
+            case "INCREMENT":
+                return state + 1;
+            case "DECREMENT":
+                return state - 1;
+            default:
+                return state;
+        }
+    }
+
+    const [state, dispatch] = useReducer(reducer, 0);
 
     function onIncrease() {
-        setNumber(++number);
+        dispatch({ type: "INCREMENT" });
     }
 
     function onDecrease() {
-        setNumber(--number);
+        dispatch({ type: "DECREMENT" });
     }
 
     return (
         <div>
-            <h1>{number}</h1>
+            <h1>{state}</h1>
             <button onClick={onIncrease}>+1</button>
             <button onClick={onDecrease}>-1</button>
         </div>
